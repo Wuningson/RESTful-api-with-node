97461c36f773d2f8a00e7e8e207dcfa3e146d01c
Wuningson <christdam55@gmail.com>
2019-03-10 09:21:13 +0100

    First commit for the project with orders and products routes set and requests for both set. Error handling

---
+++ api/routes/orders.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+//handling incoming get requests to /orders
+router.get('/', (req, res, next)=> {
+    res.status(200).json({
+        message: 'Orders were fetched'
+    });
+});
+
+//hanling incoming post requests to /orders
+router.post('/', (req, res, next)=>{
+  res.status(201).json({
+      message: 'Orders were created'
+  });  
+});
+
+//handling incoming get requests to /order/orderId
+router.get('/:orderId', (req, res, next)=> {
+    res.status(200).json({
+        message: 'Order details',
+        orderId: req.params.orderId
+    });
+});
+
+//handling incoming delete requests to /order/orderId
+router.delete('/:orderId', (req, res, next)=> {
+    res.status(200).json({
+        message: 'Order deleted',
+        orderId: req.params.orderId
+    });
+});
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan')
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
+
+//routes handling requests
+app.use('/products', productRoutes);
+
+app.use('/orders', orderRoutes);
+
+module.exports = app;
