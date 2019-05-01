b9b64ec1e335a7dd48933260e5f9f403522301cc
Wuningson <christdam55@gmail.com>
2019-05-01 16:24:52 +0100

    Added description to the products and Validation of data

--- api/models/product.js
+++ api/models/product.js
@@ -2,8 +2,18 @@ const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
 	//_id: mongoose.Schema.Types.ObjectId,
 	// It's not compulsory to make mongoose generate an id like this since it automatically generates one already
-	name: String,
-	price: Number
+	name: {
+		type: String,
+		required: true
+	},
+	price: {
+		type: Number,
+		required: true
+	},
+	description:{
+		type: String,
+		required: true
+	}
 });
 
 module.exports = mongoose.model('Product', productSchema);
--- api/routes/products.js
+++ api/routes/products.js
@@ -7,11 +7,27 @@ const mongoose = require('mongoose');
 //are forwarded to this file as such if /products is added in the get function the request url will have to /products/p
 router.get('/', (req, res, next)=>{
 	Product.find()
+	.select('name price _id')
+	//This function select basically selects the keys provided and their values to be returned instead of the entire
+	//object
 	.exec()
 	.then(prods=>{
-		console.log(prods);
+		const response = {
+			count: prods.length,
+			products: prods.map(doc=>{
+				return {
+					name: doc.name,
+					price: doc.price,
+					_id: doc._id,
+					request: {
+						type: 'GET',
+						url: 'http://localhost:3000/products/' + doc._id
+					}
+				}
+			})
+		}
 		if (prods.length>0){
-			res.status(200).json(prods);
+			res.status(200).json(response);
 		}else{
 			res.status(404).json({
 				message: "No product found"
@@ -27,9 +43,10 @@ router.get('/', (req, res, next)=>{
 //when posting its better to send a status code of 201
 router.post('/', (req, res, next)=>{
 	const product = new Product({
-		_id: new mongoose.Types.ObjectId,
+		//_id: new mongoose.Types.ObjectId,
 		name: req.body.name,
-		price: req.body.price
+		price: req.body.price,
+		description: req.body.description
 	});
 	//Great care should be taken to ensure that the status for success is sent in the promise. This is because Promises
 	//execute asynchronously as such whether it fails or succeeds the status gets executed.
@@ -37,8 +54,16 @@ router.post('/', (req, res, next)=>{
 	.then(result=>{
 		console.log(result);
 		res.status(201).json({
-			message: "Handling posts requests to /products",
-			createdProduct: result
+			message: "Created product successfully",
+			createdProduct: {
+				name: result.name,
+				price: result.price,
+				_id: result._id,
+				request: {
+					type: 'GET',
+					url: "http://localhost:3000/products/" + result._id
+				}
+			}
 		});
 	})
 	.catch(err=>{
@@ -50,12 +75,19 @@ router.post('/', (req, res, next)=>{
 router.get('/:productId', (req, res, next)=>{
 	const id = req.params.productId;
 	Product.findById(id)
+	.select('name price _id description')
 	.exec()
 	.then(doc=>{
 		console.log(doc);
 //if(doc !==null){...} will also work here because that will basically check if it exists i.e. if it is not null
 		if (doc !== null){
-			res.status(200).json({doc});
+			res.status(200).json({
+				product: doc,
+				request:{
+					type: 'GET',
+					url: 'http://localhost:3000/products'
+				}
+			});
 		}else{
 			res.status(404).json({
 				message: `${id} is not a valid ID`
@@ -72,6 +104,7 @@ router.patch('/:productId', (req, res, next)=>{
 	const id = req.params.productId;
 	const newName = req.body.newName;
 	const newPrice = req.body.newPrice;
+	const newDescription = req.body.description;
 	//The for loop here is to ensure that when there are no values passed into the body for the update the patch request
 	//won't be executed since the body will be empty
 	// for (const ops in req.body){
@@ -87,28 +120,56 @@ router.patch('/:productId', (req, res, next)=>{
 	//Instead of the for loop which will require an array input this other lengthy but quite 
 	//straightforward method can be used. The only disadvantage is that it will patch one by one
 	var newValues ={}
-	if (newPrice !== undefined && newName !== undefined){
+	if (newPrice !== undefined && newName !== undefined && newDescription !== undefined){
 		newValues = {$set: {
 			name: newName,
-			price: newPrice
+			price: newPrice,
+			description: newDescription
 		}};
-	}else if(newPrice !== undefined && newName == undefined){
+	}else if(newPrice == undefined && newName !== undefined && newDescription !== undefined){
+		newValues ={$set:{
+			name: newName,
+			description: newDescription
+		}
+	};
+	}else if(newPrice !== undefined && newName == undefined && newDescription !== undefined){
+		newValues ={$set:{
+			name: newName,
+			price: newPrice
+		}
+	};
+	}else if(newPrice !== undefined && newName == undefined && newDescription !== undefined){
+		newValues ={$set:{
+			description: newDescription,
+			price: newPrice
+		}
+	};
+	}else if(newPrice !== undefined && newName == undefined && newDescription == undefined){
 		newValues ={$set:{
 			price: newPrice
 		}
 	};
-	}else if (newName !== undefined && newPrice == undefined){
+	}else if (newName !== undefined && newPrice == undefined && newDescription == undefined){
 		newValues ={
 			$set:{
 				name: newName
 			}
 		};		
-	}
+	}else if(newPrice == undefined && newName == undefined && newDescription !== undefined){
+		newValues ={$set:{
+			description: newDescription
+		}
+	};
 	Product.updateOne({_id: id}, newValues)
 	.exec()
 	.then(updated=>{
-		console.log(updated);
-		res.status(200).json(updated);
+		res.status(200).json({
+			message: 'Product updated',
+			request: {
+				type: 'GET',
+				url: 'http://localhost:3000/products/' + id
+			}
+		});
 	})
 	.catch(err=>{
 		console.log(err);
@@ -116,6 +177,7 @@ router.patch('/:productId', (req, res, next)=>{
 			error: err
 		});
 	});
+	}
 });
 
 router.delete('/:productId', (req, res, next)=>{
@@ -124,7 +186,14 @@ router.delete('/:productId', (req, res, next)=>{
 	.exec()
 	.then(prod=>{
 		prod.remove();
-		res.status(200).json(`The product ${prod.name} has been removed`);
+		res.status(200).json({
+			message: 'Product deleted',
+			request:{
+				type: 'POST',
+				url: 'http://localhost:3000/products',
+				body: {name: 'String', price: 'Number'}
+			}
+		});
 	})
 	.catch(err=>{
 		console.log(err);
